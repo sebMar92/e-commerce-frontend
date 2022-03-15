69e9f6e221956aa9b7942202602721ff01853fbf
Matias Valdez <matiasvaldez8184@gmail.com>
2022-03-15 18:32:27 -0300

    test

---
+++ client/tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        lora: "'Lora',serif",
+        dm: "'DM Mono', monospace"
+      },
+      screens: {
+        xs: '500px',
+        xxl: '1800px'
+      },
+      colors: {
+        primary: {
+          100: '#FFE9CF',
+          200: '#FFDAAF',
+          300: '#FFCF96',
+          400: '#FEBD70',
+          500: '#FFAC4B',
+          600: '#FFA438',
+          700: '#FF9921',
+        },
+        secondary: {
+          100: '#EEEEEE',
+          200: '#808080',
+          500: '#535252',
+          700: '#424242',
+          900: '#2E2D2D',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
