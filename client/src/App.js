bae95c22d34f1d03237a9f5a5ad431ca84b1b4a1
Bufalito <93780179+Bufalito@users.noreply.github.com>
2022-03-15 15:14:31 -0300

    commit colision

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
+import {Route, Routes, BrowserRouter} from "react-router-dom"
+import Footer from "./Footer/Footer";
 function App() {
-  return <div className="App">TEST</div>;
+  /* return <div className="App">TEST</div>; */
+  return(
+    <BrowserRouter>
+    <div>
+    <Routes>
+      <Route exact path="/" element={<Footer/>}/>
+    </Routes>
+    </div>
+    </BrowserRouter>
+
+  )
 }
 
 export default App;
