4edd1470f39776bc41765cbedad9affb943e3a0e
Bufalito <93780179+Bufalito@users.noreply.github.com>
2022-03-15 17:19:52 -0300

    Footer Final

--- client/src/components/Footer/GeneralFooter.js
+++ client/src/components/Footer/GeneralFooter.js
@@ -12,33 +12,33 @@ export default function GeneralFooter() {
         <div>
             <footer >
 
-                <div className="bg-gray-300 flex sm:flex-row flex-wrap justify-evenly p-4">
-                    <div>
-                        <h4>Need help?</h4>
-                        <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=DmwnWsLPtkNmvlMqVnDjlPPRxjmPzjBJnXlZqFVwPjHtQNJJcsVgxbtxnXfQBwGtGTzpnCWrmxLQ" className="no-underline text-black"><h6 className="flex"><CgMail />Contact us</h6></a>
+                <div className="bg-gray-300 flex sm:flex-row flex-col flex-wrap justify-evenly p-4 content-center">
+                    <div className="m-2">
+                        <h3 className="flex">Need help?</h3>
+                        <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=DmwnWsLPtkNmvlMqVnDjlPPRxjmPzjBJnXlZqFVwPjHtQNJJcsVgxbtxnXfQBwGtGTzpnCWrmxLQ" className="no-underline text-black"><h5 className="flex m-2"><CgMail />Contact us</h5></a>
                     </div>
 
-                    <div>
-                        <h4>About us</h4>
-                        <a href="https://github.com/" className="no-underline text-black"><h6 className="flex"><AiFillGithub />GitHub</h6></a>
-                        <a href="https://www.linkedin.com/feed/" className="no-underline text-black"><h6 className="flex"><AiOutlineLinkedin />Linkedin</h6></a>
-
-                    </div>
-
-                    <div>
-                        <h4>Follow us</h4>
-
-                        <a href="" className="no-underline text-black"><h6 className="flex"><AiOutlineFacebook />Facebook</h6></a>
-                        <a href="" className="no-underline text-black"><h6 className="flex"><AiOutlineTwitter />Twitter</h6></a>
-                        <a href="" className="no-underline text-black"><h6 className="flex"><AiOutlineInstagram />Instagram</h6></a>
+                    <div className="m-2">
+                        <h3 className="flex">About us</h3>
+                        <a href="https://github.com/" className="no-underline text-black"><h5 className="flex m-2"><AiFillGithub />GitHub</h5></a>
+                        <a href="https://www.linkedin.com/feed/" className="no-underline text-black"><h6 className="flex m-2"><AiOutlineLinkedin />Linkedin</h6></a>
 
                     </div>
 
-                    <div>
-                        <h4>My account</h4>
-                        <Link to={`/user/${id}`} className="no-underline text-black"><h6 className="flex"><CgProfile />Profile</h6></Link>
-                        <Link to={`/wishlist/${id}`} className="no-underline text-black"><h6 className="flex"><MdFavorite />Favorites</h6></Link>
-                        <Link to={`/historial/${id}`} className="no-underline text-black"><h6 className="flex"><MdOutlineShoppingBag />Shopps</h6></Link>
+                    <div className="m-2">
+                        <h3 className="flex">Follow us</h3>
+
+                        <a href="" className="no-underline text-black"><h5 className="flex m-2"><AiOutlineFacebook />Facebook</h5></a>
+                        <a href="" className="no-underline text-black"><h5 className="flex m-2"><AiOutlineTwitter />Twitter</h5></a>
+                        <a href="" className="no-underline text-black"><h5 className="flex m-2"><AiOutlineInstagram />Instagram</h5></a>
+
+                    </div>
+
+                    <div className="m-2">
+                        <h3 className="flex">My account</h3>
+                        <Link to={`/user/${id}`} className="no-underline text-black"><h5 className="flex m-2"><CgProfile />Profile</h5></Link>
+                        <Link to={`/wishlist/${id}`} className="no-underline text-black"><h5 className="flex m-2"><MdFavorite />Favorites</h5></Link>
+                        <Link to={`/historial/${id}`} className="no-underline text-black"><h5 className="flex m-2"><MdOutlineShoppingBag />Shopps</h5></Link>
                     </div>
                 </div>
                 <p className="bg-gray-300 text-center mt-3 p-1">© 2022 by Nombre e-commerce</p>
