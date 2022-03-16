dd3ac3718d234556f69ff021101ff619d8240670
Bufalito <93780179+Bufalito@users.noreply.github.com>
2022-03-16 18:39:17 -0300

    ComponentFilterAndOrden

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,16 +3,17 @@ import NavBar from './NavBar';
 import Footer from './Footer/Footer';
 import Card from './Card';
 
+
+
 export default function Home() {
     return (
         <>
         <NavBar/>
         <div className='home'>
-            
             <h1>This is home</h1>
            <Card/>
         </div>
         <Footer/>
         </>
     );
-}   
+}
--- client/src/components/ProductsByCategory.js
+++ client/src/components/ProductsByCategory.js
@@ -2,16 +2,22 @@ import React from 'react';
 import NavBar from './NavBar';
 import Footer from './Footer/Footer';
 import Card from './Card';
+import FilterAndOrderComponent from './FilterAndOrden';
 
 export default function Products() {
     return (
         <>
-        <NavBar/>
-        <div className='products'>
-            <h1>This is products by category</h1>
-            <Card/>
-        </div>
-        <Footer/>
+            <NavBar />
+            <div className='flex flex-col sm:flex-row'>
+                <FilterAndOrderComponent />
+                <div className='products'>
+                    <h1>This is products by category</h1>
+                    <Card />
+                </div>
+            </div>
+            <Footer />
         </>
     );
 }
+
+
