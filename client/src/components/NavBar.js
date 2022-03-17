f46d4a8cb5caa110f06a2a88bd46d348d9827f7f
Matias Valdez <matiasvaldez8184@gmail.com>
2022-03-17 15:45:45 -0300

    Añadi estilos a la navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,31 +2,32 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "./commons/SearchBar";
 import DropDown from "./DropDown";
-import NightModeButton from "./NightModeButton";
+import NightModeButton from "./commons/NightModeButton";
 
 export default function NavBar() {
   const arrayPrueba = ["hola", "hello"];
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   const hidden = "hidden";
 
   return (
-    <div class="bg-primary-500 font-medium text-md px-4 py-2">
-      <div className="flex flex-direction:row justify-between">
+    <div class="bg-primary-500 font-medium text-lg px-4 py-2">
+      <div className="flex flex-row justify-between">
         <div>
           <Link to="/">
-            <div class="flex space-x-2 text-decoration-line: no-underline">
+            <div class="flex space-x-2 no-underline">
               <img
                 src="https://img.icons8.com/external-filled-outline-icons-maxicons/344/external-tech-future-of-technology-filled-outline-icons-maxicons.png"
                 alt="company icon"
                 class="flex-none h-10 w-10"
               ></img>
-              <h3 class=" text-black text-decoration-line: no-underline">
+              <h3 class=" text-black no-underline">
                 Company Name
               </h3>
             </div>
           </Link>
         </div>
+        <NightModeButton />
         <div class="block lg:hidden">
           <button
             class="flex items-center px-3 py-2 rounded"
@@ -49,20 +50,20 @@ export default function NavBar() {
           <SearchBar class="" />
         </div>
       </div>
-
+    <div className="flex justify-center">
       <div
-        className={`bg-primary-300 rounded m-2 w-10/12 mg:hidden lg:flex flex-direction:row justify-between ${
+        className={`bg-primary-300 rounded m-2 w-10/12 sm:hidden lg:flex flex-row justify-between z-10 ${
           isOpen && hidden
         } `}
       >
-        <DropDown tittle="Favorites" array={arrayPrueba} />
-        <DropDown tittle="Cart" array={arrayPrueba} />
-        <NightModeButton />
+        <DropDown tittle="News" array={arrayPrueba} />
+        <DropDown tittle="Historial" array={arrayPrueba} />
         <DropDown tittle="User" array={arrayPrueba} />
         <DropDown tittle="Category" array={arrayPrueba} />
-        <DropDown tittle="Historial" array={arrayPrueba} />
-        <DropDown tittle="Notices" array={arrayPrueba} />
+        <DropDown tittle="Cart" array={arrayPrueba} />
+        <DropDown tittle="Favorites" array={arrayPrueba} />
       </div>
+    </div>
     </div>
   );
 }
