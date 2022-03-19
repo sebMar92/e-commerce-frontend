29ceb59e968bf93a50da874300360039c5a6baa7
Alejandro Quintero Mejía <alejandroquinteromejia@gmail.com>
2022-03-19 16:44:59 -0500

    noje

--- client/src/components/CarouselCateg.js
+++ client/src/components/CarouselCateg.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useSelector, useEffect, useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Slider from "react-slick"
 import { Link } from 'react-router-dom';
 import { getProductsByCategory } from '../Redux/Actions/actions';
