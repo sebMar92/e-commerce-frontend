b15c2f1f2988ca1972842a1698cac695b4a6cbe9
FelipeCiroM <felipe.cirom@udea.edu.co>
2022-03-19 23:30:39 -0500

    carousel-march-19

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,31 +1,20 @@
-import React from 'react';
-import NavBar from './NavBar'
-import CarouselPromo from './CarouselPromo';
-import CarouselCateg from './CarouselCateg';
-import Footer from './Footer/Footer';
-import CardHome from './CardHome';
-
-
+import React from "react";
+import NavBar from "./NavBar";
+import CarouselPromo from "./CarouselPromo";
+import CarouselCateg from "./CarouselCateg";
+import Footer from "./Footer/Footer";
+import CardHome from "./CardHome";
 
 export default function Home() {
-    return (
-        <>
-        <NavBar/>
-        <CarouselPromo />
-        <div className='md:my-32'>
-            <h1 className="flex justify-center">Category1</h1>
-            <CarouselCateg />
-        </div>
-        <div className='my-32'>
-            <h1 className="flex justify-center">Category2</h1>
-            <CarouselCateg />
-        </div>
-        <div className='my-32'>
-            <h1 className="flex justify-center">Category3</h1>
-            <CarouselCateg />
-        </div>
-        <Footer/>
-        </>
-    );
-}   
+  return (
+    <>
+      <NavBar />
+      <CarouselPromo />
+      <div className="md:my-32">
+        <CarouselCateg />
+      </div>
 
+      <Footer />
+    </>
+  );
+}
