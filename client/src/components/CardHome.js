b02c2c1639e41d63a9d6aadfbba86a9b061f0802
Alejandro Quintero Mejía <alejandroquinteromejia@gmail.com>
2022-03-21 15:00:05 -0500

    botones1

---
+++ client/src/components/CardHome.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Star from "./utils/star-regular-24.png";
+import Cart from "./utils/cart-alt-solid-24.png";
+
+export default function CardHome({ id, image, title, price }) {
+  return (
+    <Link to={`/product/${id}`} className="no-underline">
+      <div className="p-10">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <div className="flex justify-center">
+            <img className="h-36" src={image} alt="img" />
+          </div>
+
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2"></div>
+            <p className="font-semibold tracking-tight h-36 text-gray-900 dark:text-white text-base">
+              {title}
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="flex flex-wrap justify-around">
+              <Link to={"/wishlist/:idUser"}>
+                <img className="p-2 hover:w-10" src={Star} alt="img" />
+              </Link>
+              <Link to={"/cart/:idUser"} style={{ textDecoration: "inherit" }}>
+                <img className="p-2 hover:w-10" src={Cart} alt="img" />
+              </Link>
+            </span>
+            <br />
+            <span className="inline-block text-3xl font-bold text-gray-900 mr-2 mb-2 dark:text-white">
+              US $ {price} 
+            </span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
---
+++ client/src/components/CarouselCateg.js
@@ -0,0 +1,295 @@
+import React from "react";
+import Slider from "react-slick"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts, getCategories } from "../Redux/Actions/actions";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import CardHome from "./CardHome";
+
+export default function CarouselCateg() {
+  const dispatch = useDispatch();
+  const allProducts = useSelector((state) => state.home.products);
+  const allCategories = useSelector((state) => state.home.categories);
+
+
+  
+  let arr = [];
+  for (let i = 0; i < allCategories.length; i++) {
+    arr.push({ id: i + 1, name: allCategories[i] });
+  }
+
+  useEffect(() => {
+    dispatch(getProducts("?limit=100"));
+    dispatch(getCategories());
+  }, []);
+
+  function randomCategories(array) {
+    var categories = [];
+    categories = [...array]
+      .sort(() => (Math.random() > 0.5 ? 1 : -1))
+      .slice(0, 3);
+    return categories;
+  }
+
+  function prod(allProducts, categ) {
+    if (allProducts) {
+      var b = [];
+      for (let i = 0; i < allProducts.length; i++) {
+        for (let j = 0; j < allProducts[i].categories.length; j++) {
+          if (allProducts[i].categories[j].id === categ.id) {
+            b.push(allProducts[i]);
+          }
+        }
+      }
+      if (b.length === 1){
+        var settings = {
+          dots: true,
+          infinite: false,
+          speed: 500,
+          slidesToShow: 1,
+          nextArrow: <SampleNextArrow />,
+          prevArrow: <SamplePrevArrow />,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                infinite: false,
+                dots: true,
+              },
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                infinite: false,
+                dots: true,
+              },
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                infinite: false,
+                dots: true,
+              },
+            },
+          ],
+        };        
+      }
+      if(b.length === 2){
+        var settings = {
+          dots: true,
+          infinite: false,
+          speed: 500,
+          slidesToShow: 2,
+          /* slidesToScroll: 1, */
+          nextArrow: <SampleNextArrow />,
+          prevArrow: <SamplePrevArrow />,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                infinite: false,
+                dots: true,
+              },
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                infinite: false,
+                dots: true,
+              },
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+              },
+            },
+          ],
+        };        
+      }
+      if(b.length === 3){
+        var settings = {
+          dots: true,
+          infinite: false,
+          speed: 500,
+          slidesToShow: 3,
+          nextArrow: <SampleNextArrow />,
+          prevArrow: <SamplePrevArrow />,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                infinite: false,
+                dots: true,
+              },
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+              },
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+              },
+            },
+          ],
+        };        
+      }
+      if (b.length >= 4){
+        var settings = {
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          nextArrow: <SampleNextArrow />,
+          prevArrow: <SamplePrevArrow />,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+              },
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true,
+              },
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+              },
+            },
+          ],
+        }; 
+      }
+    }
+    return b;
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div className="flex flex-start top-1/2 cursor-pointer">
+        <button
+          className="absolute bg-orange-500 text-white p-1.5 rounded-full bg-opacity-30 cursor-pointer hover:bg-opacity-60 transition sm:p-5 text-lg md:p-7 md:text-xl lg:p-7 lg:text-3xl lg:font-bold top-1/2 cursor-pointer text-center  right-full"
+          onClick={onClick}
+        >
+          <AiOutlineLeft />
+        </button>
+      </div>
+    );
+  }
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div className="flex flex-start top-1/2 cursor-pointer"> 
+        <button
+          className="absolute bg-orange-500 text-white p-1.5 rounded-full bg-opacity-30 cursor-pointer hover:bg-opacity-60 transition sm:p-5 text-lg md:p-7 md:text-xl lg:p-7 lg:text-3xl lg:font-bold top-1/2 cursor-pointer text-center  left-full right-4/" 
+          onClick={onClick}
+        >
+          <AiOutlineRight />
+        </button>
+      </div>
+    );
+  }
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  };
+
+
+
+  return (
+    <div className="max-w-screen-lg m-auto mt-3 sm:mt-5">
+      {randomCategories(arr).map((categ) => {
+        return (
+          <div className="font-lora text-center text-xs sm:text-lg md:text-xl lg:text-2xl font-bold ">
+            <h1 className="my-16">{categ.name}</h1>
+            <Slider {...settings}>
+              {prod(allProducts, categ).map((product) => {
+                return (
+                  <div>
+                    <CardHome
+                      key={product.id}
+                      id={product.id}
+                      image={product.images[0].url}
+                      title={product.title}
+                      price={product.price}
+                    />
+                  </div>
+                );
+              })}
+              </Slider>
+            </div>
+        );
+      })}
+    </div>
+  );
+}
+<br />;
