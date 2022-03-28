97f2ece40a91b8647b16370565eba9d76277b044
unknown <sebmar92@gmail.com>
2022-03-28 07:15:05 -0300

    buy

--- client/src/Redux/Reducers/HomeReducer.js
+++ client/src/Redux/Reducers/HomeReducer.js
@@ -11,8 +11,8 @@ import {
   DELETE_ORDERS,
   PUT_USER_INFO,
   PUT_ORDERS,
-  PUT_ORDERS_AMOUNT
-} from "../Actions/types";
+  PUT_ORDERS_AMOUNT,
+} from '../Actions/types';
 
 const initialState = {
   products: [],
@@ -23,7 +23,7 @@ const initialState = {
   userInfo: [],
   userTokens: [],
   userMail: [],
-  openFiles: "",
+  openFiles: '',
   answer: {},
   user: {},
   inWishList: [],
@@ -32,7 +32,7 @@ const initialState = {
   postOrders: [],
   deleted: [],
   resPutOrder: [],
-  resAmountOrder:[]
+  resAmountOrder: {},
 };
 export const HomeReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -71,18 +71,18 @@ export const HomeReducer = (state = initialState, action) => {
     case DELETE_ORDERS:
       return {
         ...state,
-        deleted: action.payload
-      }
+        deleted: action.payload,
+      };
     case POST_ORDERS:
       return {
         ...state,
-        postOrders: action.payload
-      }
+        postOrders: action.payload,
+      };
     case GET_ORDERS:
       return {
         ...state,
         [action.payload.status]: action.payload.data,
-      }
+      };
     case GET_USER_INFO:
       return {
         ...state,
@@ -92,18 +92,18 @@ export const HomeReducer = (state = initialState, action) => {
       return {
         ...state,
         answer: action.payload,
-      }
+      };
 
     case PUT_ORDERS:
       return {
         ...state,
-        resPutOrder: action.payload
+        resPutOrder: action.payload,
       };
-      case PUT_ORDERS_AMOUNT:
-        return {
-          ...state,
-          resAmountOrder: action.payload
-        }
+    case PUT_ORDERS_AMOUNT:
+      return {
+        ...state,
+        resAmountOrder: action.payload,
+      };
 
     default:
       return state;
