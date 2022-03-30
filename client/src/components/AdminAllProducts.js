1c4bafe18af7819c620d6717137134315e04b457
LucMuno <lucianommunoz@gmail.com>
2022-03-30 00:14:47 -0300

    searchbarOK

--- client/src/components/AdminAllProducts.js
+++ client/src/components/AdminAllProducts.js
@@ -2,16 +2,59 @@ import React from 'react';
 import Footer from './Footer/Footer';
 import NavbarAdmin from './NavbarAdmin';
 import NavBarEmpty from './NavBarEmpty';
+import { useLocation } from 'react-router-dom';
+import SearchBarAdmin from "./commons/SearchBarAdmin";
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { getCategories, getProducts } from "../Redux/Actions/actions";
+import DropDownCategories from "./DropDownCategories";
+import CardAdmin from './CardAdmin';
 
 export default function AdminAllProducts() {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const search = location.search;
+  const allProducts = useSelector((state) => state.home.products);
+  const allCategories = useSelector((state) => state.home.categories);
+  const products = useSelector((state) => state.home.products);
+  useEffect(() => {
+    dispatch(getProducts(search));
+    dispatch(getCategories());
+  }, [search]);
+  
+  useEffect(() => {
+        dispatch(getCategories());
+  }, []);
+    
+    console.log('aqui', products)  
     return (
             <>
             <NavBarEmpty/>
             <div className='flex flex-col sm:flex-row' >
                 <NavbarAdmin />
                 <div className='m-auto'>
-                    <h1>Here admin can see all the products
-                    </h1>
+                <DropDownCategories tittle="Categories" array={allCategories} />
+                    <SearchBarAdmin/>
+                    {/*<h1>Here admin can see all the products
+                    </h1>*/}
+                     {allProducts && 
+                      allProducts.map((prod) => {
+                       console.log(prod)
+                      return (
+                       <div>
+ 
+                           <CardAdmin 
+                            key={prod.id}
+                            title={prod.title}
+                            price={prod.price}
+                            images={prod.images[0].url}
+                            id={prod.id}
+                               
+                             />
+                       </div>
+                        );
+                    })}
+
                 </div>
             </div>
             </>
---
+++ client/src/components/commons/SearchBarAdmin.js
@@ -0,0 +1,139 @@
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate, createSearchParams } from "react-router-dom";
+import { getProducts, getSearch } from "../../Redux/Actions/actions";
+import CardAdmin from '../CardAdmin'
+
+export default function SearchBar(props) {
+  const [isOpen, setIsOpen] = useState(true); //controla que aparezca y desaparezca el autocomplete
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.home.search.products);
+  const categories = useSelector((state) => state.home.search.categories);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setSearchValue(value);
+    if (value !== "") {
+      dispatch(getSearch(value));
+    }
+  };
+  useEffect(() => {
+    dispatch(getProducts(searchValue));
+  },[searchValue])
+
+  /*function handleSubmit(e){
+    e.preventDefault();
+    dispatch(getProducts(searchValue));
+    
+  }*/
+  console.log('products', products)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('valor', searchValue)
+    if (searchValue !== "") {
+      navigate({
+        pathname: `/admin/products?search=${searchValue}`,
+      });
+      setSearchValue("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex justify-center font-lora">
+      <input
+        onChange={(e) => handleInputChange(e)}
+        onFocus={() => setIsOpen(false)}
+        value={searchValue}
+        className="bg-secondary-100 p-2 h-8 rounded-md w-10/12 md:w-4/12 focus:outline-none"
+        placeholder="Type to search..."
+      />
+      <button
+        type="submit"
+        className="text-secondary-200 bg-secondary-100 p-1 ml-1 rounded-md active:translate-y-1"
+      >
+        <AiOutlineSearch />
+      </button>
+      <div
+        className={`absolute mt-10 bg-secondary-100 m-2 overflow-hidden rounded-lg shadow-lg z-20 w-10/12 md:w-4/12 overflow-y-auto max-h-96 overflow-x-auto  ${
+          (isOpen || searchValue === "") && "hidden"
+        }`}
+      >
+       {/*} {categories &&
+          categories.map((category) => {
+            return (
+              <div className="text-black" key={category.id}>
+                <Link
+                  to={`/products?categoryId=${category.id}&offset=1`}
+                  className="text-decoration-line: no-underline"
+                >
+                  <div className="hover:bg-primary-300 flex gap-4 p-4">
+                    <img
+                      src={category.image}
+                      alt={category.name}
+                      className="w-12 h-12 object-contain"
+                    />
+                    <div className="self-center">
+                      <h1 className="text-sm font-bold text-black ">
+                        Category: {category.name}
+                      </h1>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+          {products && products.products &&
+        products.products.map((prod) => {
+          console.log(prod)
+          return (
+            <div>
+ 
+              <CardAdmin 
+               key={prod.id}
+               title={prod.title}
+               price={prod.price}
+               images={prod.images[0].url}
+               id={prod.id}
+                
+                
+              />
+            </div>
+          );
+        })}*/}
+        {products &&
+          products.products &&
+          products.products.map((product) => {
+            return (
+              <div key={product.id}>
+                <Link
+                  to={`/admin/edit/${product.id}`}
+                  className="text-decoration-line: no-underline"
+                >
+                  <div className="hover:bg-primary-300 flex gap-4 p-4">
+                    
+                    <img
+                      src={product.images.url && product.images[0].url}
+                      alt={product.title}
+                      className="w-12 h-12 object-contain"
+                    />
+                    <div>
+                      <h3 className="text-sm font-semibold text-black">
+                        {product.title}
+                      </h3>
+                      <p className="text-xs text-gray-600">
+                        Price: US $ {product.price}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+      </div>
+    </form>
+  );
+}
