31140b5095cd8a581a5f26b4de1b14a3ef46a2af
Yesmin Arias <yyariase@gmail.com>
2022-03-31 09:13:37 -0500

    graficos

--- client/src/components/AdminProfile.js
+++ client/src/components/AdminProfile.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import Footer from './Footer/Footer';
-import NavbarAdmin from './NavbarAdmin';
-import NavBarEmpty from './NavBarEmpty';
+import React, { useEffect } from "react";
+import Footer from "./Footer/Footer";
+import NavbarAdmin from "./NavbarAdmin";
+import NavBarEmpty from "./NavBarEmpty";
+import { useDispatch, useSelector } from "react-redux";
+import { getOrder } from "../Redux/Actions/actions";
 
 
 export default function AdminProfile() {
-    return (
-        <>
-        <NavBarEmpty/>
-        <div className='flex flex-col sm:flex-row' >
-            <NavbarAdmin />
-            <div className='m-auto'>
-                <h1>This is Admin's profile.
-                It shows recent users, best sellers,
-                latest shoppings
-                </h1>
-            </div>
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.home.finished);
+  const user = useSelector((state) => state.home.userInfo);
+  console.log(user);
+
+  useEffect(() => {
+    dispatch(getOrder({ status: "finished" }));
+  });
+
+  return (
+    <>
+      <NavBarEmpty />
+      <div className="flex flex-col sm:flex-row">
+        <NavbarAdmin />
+        
+          <h1 className="mx-auto text-center"> Admin's profile </h1>
+          <div>
+
+          </div>
         </div>
-        </>
-    );
+      
+    </>
+  );
 }
