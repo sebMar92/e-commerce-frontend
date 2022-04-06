156cd79103258ef1fb8b41407af2a44a39db047d
FelipeCiroM <felipe.cirom@udea.edu.co>
2022-04-06 01:11:49 -0500

    links

--- client/src/components/CarouselPromo.js
+++ client/src/components/CarouselPromo.js
@@ -14,6 +14,7 @@ export default function Carousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const sales = useSelector((state) => state.admin.sales);
   /* sales && console.log("sales: " + sales[1].categories[0].id); */
+  sales.length && console.log("products: " + sales[0].products[0].id);
   const dispatch = useDispatch();
   const slideRef = useRef();
   useEffect(() => {
@@ -55,6 +56,12 @@ export default function Carousel() {
   const removeAnimation = () => {
     slideRef.current.classList.remove("fade-anim");
   };
+  function aleatorio(inferior, superior) {
+    var numPosibilidades = superior - inferior;
+    var aleatorio = Math.random() * (numPosibilidades + 1);
+    aleatorio = Math.floor(aleatorio);
+    return inferior + aleatorio;
+  }
 
   return (
     <div className="w-[65vw] m-auto mt-3 sm:mt-5 shadow-2xl shadow-orange-300">
@@ -63,8 +70,38 @@ export default function Carousel() {
         className="w-full relative select-none shadow-md shadow-slate-600 sm:rounded"
       >
         {sales.length ? (
-          sales[currentIndex].products[0].id && (
-            <Link to={`/products/${sales[currentIndex].products[0].id}`}>
+          sales[currentIndex].products.length ? (
+            <Link to={`/product/${sales[currentIndex].products[0].id}`}>
+              <div className="aspect-w-9 aspect-h-4">
+                <img
+                  className="sm:rounded"
+                  src={sales.length && sales[currentIndex].image}
+                  alt=""
+                />
+                <span className="font-lora text-center text-lg  font-bold top-1/2 text-white bg-black bg-opacity-70 h-8 mt-auto mb-6">
+                  {sales.length && sales[currentIndex].description}
+                </span>
+              </div>
+            </Link>
+          ) : sales[currentIndex].categories.length ? (
+            sales[currentIndex].categories[0].id !== 0 && (
+              <Link
+                to={`/products?categoryId=${sales[currentIndex].categories[0].id}&offset=1`}
+              >
+                <div className="aspect-w-9 aspect-h-4">
+                  <img
+                    className="sm:rounded"
+                    src={sales.length && sales[currentIndex].image}
+                    alt=""
+                  />
+                  <span className="font-lora text-center text-lg  font-bold top-1/2 text-white bg-black bg-opacity-70 h-8 mt-auto mb-6">
+                    {sales.length && sales[currentIndex].description}
+                  </span>
+                </div>
+              </Link>
+            )
+          ) : (
+            <Link to={`/products?categoryId=${aleatorio(1, 6)}&offset=1`}>
               <div className="aspect-w-9 aspect-h-4">
                 <img
                   className="sm:rounded"
@@ -77,7 +114,10 @@ export default function Carousel() {
               </div>
             </Link>
           )
-        ) : sales[currentIndex].categories.length ? (
+        ) : (
+          <br />
+        )}
+        {/* {sales.length && sales[currentIndex].categories.length ? (
           sales[currentIndex].categories[0].id !== 0 ? (
             <Link
               to={`/products?categoryId=${sales[currentIndex].categories[0].id}&offset=1`}
@@ -116,7 +156,7 @@ export default function Carousel() {
               {sales.length && sales[currentIndex].description}
             </span>
           </div>
-        )}
+        )} */}
         <div className="hidden cursor-default absolute w-full top-1/2 transform -translate-y-1/2 md:flex md:justify-between items-start px-3">
           <button
             className="text-primary-800 p-1.5 rounded-full bg-opacity-30 cursor-pointer hover:bg-opacity-60 transition sm:p-5 text-lg md:p-7 md:text-xl lg:p-7 lg:text-3xl lg:font-bold -translate-x-28 hover:scale-110"
