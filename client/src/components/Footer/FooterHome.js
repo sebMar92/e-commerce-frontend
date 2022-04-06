74f7627fd473577b5963765a4b6ce8d3394b1a55
unknown <sebmar92@gmail.com>
2022-04-06 16:15:28 -0300

    sub button changes

--- client/src/components/Footer/FooterHome.js
+++ client/src/components/Footer/FooterHome.js
@@ -1,48 +1,57 @@
-import React from "react";
-import { useState, useEffect} from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { putUserInfo, getUserInfo } from "../../Redux/Actions/actions";
-import ButtonCreate from "../commons/ButtonCreate";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { putUserInfo, getUserInfo } from '../../Redux/Actions/actions';
+import ButtonCreate from '../commons/ButtonCreate';
 export default function FooterHome() {
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.home.user);
-    const [type, setType] = useState("Sub / Unsub");
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.home.user);
+  const [type, setType] = useState('Sub / Unsub');
 
-    useEffect(() => {
-        dispatch(getUserInfo());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getUserInfo());
+  }, [dispatch]);
 
-    function handleChangeOption(){
-        if(user.newsletterSubscription === false){
-            user.newsletterSubscription = true;
-            dispatch(putUserInfo({newsletterSubscription: true}));
-            setType("Unsubscribe")
-            alert('Subscribed!')
-        }
-        else if(user.newsletterSubscription === true){
-            user.newsletterSubscription = false;
-            dispatch(putUserInfo({newsletterSubscription: false}));
-            setType("Subscribe")
-            alert('Unsubscribed!')
-        }
+  useEffect(() => {
+    if (user.newsletterSubscription === false) {
+      setType('Subscribe');
+    } else {
+      setType('Unsubscribe');
     }
+  }, [user]);
 
-    return (
-        <div>
-        {Object.keys(user).length !== 0 &&
-            <footer className="bg-gray-50 flex flex-row flex-wrap justify-evenly items-center p-20 font-lora font-bold">
-                <div>
-                    <h1>Join our newsletter</h1>
-                    <p>Subscribe today and get more information about our products!</p>
-                </div>
-                <div>
-                    <button className="bg-primary-600 px-6 py-2 rounded-md text-lg font-lora font-bold active:translate-y-1 hover:bg-primary-500 shadow-lg shadow-primary-200/80 m-3 dark:hover:text-white dark:hover:bg-slate-900 dark:hover:shadow-slate-600 dark:bg-slate-400 dark:text-slate-900 dark:shadow-slate-900"
-                    onClick={handleChangeOption}>
-                        {type}
-                    </button>
-                </div>
-            </footer>
-        }
-        </div>
-    )
+  function handleChangeOption() {
+    if (user.newsletterSubscription === false) {
+      user.newsletterSubscription = true;
+      dispatch(putUserInfo({ newsletterSubscription: true }));
+      setType('Unsubscribe');
+      alert('Subscribed!');
+    } else if (user.newsletterSubscription === true) {
+      user.newsletterSubscription = false;
+      dispatch(putUserInfo({ newsletterSubscription: false }));
+      setType('Subscribe');
+      alert('Unsubscribed!');
+    }
+  }
+
+  return (
+    <div>
+      {Object.keys(user).length !== 0 && (
+        <footer className="bg-gray-50 flex flex-row flex-wrap justify-evenly items-center p-20 font-lora font-bold">
+          <div>
+            <h1>Join our newsletter</h1>
+            <p>Subscribe today and get more information about our products!</p>
+          </div>
+          <div>
+            <button
+              className="bg-black text-white px-6 py-2 rounded-md text-lg font-lora font-bold active:translate-y-1 hover:bg-secondary-200 m-3 dark:hover:text-white dark:hover:bg-slate-900 dark:hover:shadow-slate-600 dark:bg-slate-400 dark:text-slate-900 dark:shadow-slate-900"
+              onClick={handleChangeOption}
+            >
+              {type}
+            </button>
+          </div>
+        </footer>
+      )}
+    </div>
+  );
 }
