5f5f3b350c976a21999678c328265c51c768eeb6
unknown <sebmar92@gmail.com>
2022-04-07 01:43:23 -0300

    arreglos de momento

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,29 +1,32 @@
-import React,{useState,useEffect} from "react";
-import NavBar from "./NavBar";
-import CarouselPromo from "./CarouselPromo";
-import CarouselCateg from "./CarouselCateg";
-import Footer from "./Footer/Footer";
-import { ToastContainer, toast } from "react-toastify";
-import axios from 'axios'
-import {useSearchParams,useLocation} from 'react-router-dom'
-import SkeletonCarrousel from "./Skeletons/SkeletonCarrousel";
-import { useDispatch,useSelector } from "react-redux";
-import { getOrder,getProducts,getCategories,clearProductAndCategory } from "../Redux/Actions/actions";
-
+import React, { useState, useEffect } from 'react';
+import NavBar from './NavBar';
+import CarouselPromo from './CarouselPromo';
+import CarouselCateg from './CarouselCateg';
+import Footer from './Footer/Footer';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
+import { useSearchParams, useLocation } from 'react-router-dom';
+import SkeletonCarrousel from './Skeletons/SkeletonCarrousel';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getOrder,
+  getProducts,
+  getCategories,
+  clearProductAndCategory,
+} from '../Redux/Actions/actions';
 
 export default function Home() {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const categories = useSelector((state) => state.home.categories);
-  const [loaded,setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false);
   const notify = () => {
-    toast.success("Added to the wishlist !", {
+    toast.success('Added to the wishlist !', {
       position: toast.POSITION.BOTTOM_LEFT,
     });
   };
 
   const notify2 = () => {
-    toast.success("Added to the cart !", {
+    toast.success('Added to the cart !', {
       position: toast.POSITION.BOTTOM_LEFT,
     });
   };
@@ -31,8 +34,8 @@ export default function Home() {
   useEffect(() => {
     dispatch(getProducts());
     dispatch(getCategories());
-    dispatch(getOrder({ status: "inCart" }))
-    dispatch(getOrder({ status: "inWishList" }))
+    dispatch(getOrder({ status: 'inCart' }));
+    dispatch(getOrder({ status: 'inWishList' }));
     return () => {
       dispatch(clearProductAndCategory());
       setLoaded(false);
@@ -45,25 +48,24 @@ export default function Home() {
     }, 2000);
   }, [categories]);
 
-
-
   return (
     <>
-    <div>
-      <ToastContainer autoClose={2000} />
-      <NavBar />
-      <CarouselPromo />
-      {loaded ?
-      <div className="md:my-32">
-        <CarouselCateg onClick={notify} onClick2={notify2} />
-      </div>
-      :
-      <>
-      <SkeletonCarrousel/>
-      <SkeletonCarrousel/>
-      <SkeletonCarrousel/>
-      </>  }
-      <Footer />
+      <div>
+        <ToastContainer autoClose={2000} />
+        <NavBar />
+        <CarouselPromo />
+        {loaded ? (
+          <div className="md:my-32">
+            <CarouselCateg onClick={notify} onClick2={notify2} />
+          </div>
+        ) : (
+          <>
+            <SkeletonCarrousel />
+            <SkeletonCarrousel />
+            <SkeletonCarrousel />
+          </>
+        )}
+        <Footer />
       </div>
     </>
   );
